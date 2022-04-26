d6cc686edc7a25fa086d4a026e863188b6f3f83b
fc56273 <fc56273@alunos.fc.ul.pt>
2022-04-26 16:53:15 +0100

    fix cozinha

--- scripts/regaScript.js
+++ scripts/regaScript.js
@@ -2,5 +2,9 @@
 
 function addRega(init, end, everyMonth, fromM, toM, everyDay, fromD, toD){
     let fileData = new Blob([txt], {type: 'text/plain'});
+    if (textFileUrl !== null) {
+        window.URL.revokeObjectURL(textFile);
+    }
+    textFileUrl = window.URL.createObjectURL(fileData);
 }
 
