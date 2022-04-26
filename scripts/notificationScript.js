b400b383cccbf45671e13299f0f17c8f147867bd
MartimBaptista <martimdsbaptista@gmail.com>
2022-04-26 18:05:22 +0100

    Added buttom to remove notification

--- scripts/notificationScript.js
+++ scripts/notificationScript.js
@@ -1,8 +1,17 @@
 var noticationList = document.getElementById("notificationList");
 
-function addNotificationn() {
-    const text = document.createTextNode("Notification Created by script");
-    const item = document.createElement("li")
+function addNotification(par) {
+    const text = document.createTextNode("- " + par);
+    const item = document.createElement("li");
+    const buttom = document.createElement("input");
+    buttom.type = "button";
+    buttom.value = "OK";
+    buttom.addEventListener("click", removeNotification);
     item.appendChild(text);
+    item.append(buttom);
     noticationList.appendChild(item);
+}
+
+function removeNotification() {
+    this.parentElement.remove();
 }
