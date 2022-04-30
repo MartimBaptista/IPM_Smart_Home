a9cba43981ccfd30ecc7ccd2f808173bfec6b125
MartimBaptista <martimdsbaptista@gmail.com>
2022-04-30 18:38:15 +0100

    main styling and clock rework

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -3,14 +3,13 @@ function startTime() {
     const today = new Date();
     let h = today.getHours();
     let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+    h = addZero(h);
+    m = addZero(m);
+    document.getElementById("clock").innerHTML = h + ":" + m;
     setTimeout(startTime, 1000);
 }
 
-function checkTime(i) {
+function addZero(i) {
     if (i < 10) {i = "0" + i};
     return i;
 }
