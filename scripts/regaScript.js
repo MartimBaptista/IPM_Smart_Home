0f39995f429bb978f3b434d528c0de3d6e755d9f
Martim Paraíba <fc56273@aluno.fc.ul.pt>
2022-05-01 18:36:07 +0100

    First implementation addRega()

--- scripts/regaScript.js
+++ scripts/regaScript.js
@@ -1,10 +1,32 @@
 
+// init, end, everyMonth, fromM, toM, everyDay, fromD, toD
+function addRega(){
+    const name          = document.getElementById("name").value;
+    const init          = document.getElementById("init").value;
+    const end           = document.getElementById("end").value;
+    const everyMonth    = document.getElementById("everyMonth").value;
+    const fromM         = document.getElementById("fromM").value;
+    const toM           = document.getElementById("toM").value;
+    const everyDay      = document.getElementById("everyDay").value;
+    const fromD         = document.getElementById("fromD").value;
+    const toD           = document.getElementById("toD").value;
 
-function addRega(init, end, everyMonth, fromM, toM, everyDay, fromD, toD){
-    let fileData = new Blob([txt], {type: 'text/plain'});
-    if (textFileUrl !== null) {
-        window.URL.revokeObjectURL(textFile);
-    }
-    textFileUrl = window.URL.createObjectURL(fileData);
+    document.getElementById("demo").innerHTML = name + init + end + everyMonth + fromM + toM + everyDay + fromD + toD;
+
+    const listaRega = document.getElementById("listaRega");
+
+    li = document.createElement("li");
+    h4 = document.createElement("h4");
+    ul = document.createElement("ul");
+
+    timeP = document.createElement("p");
+    everyDayP = document.createElement("p");
+    everyMonthP = document.createElement("p");
+
+    h4.innerHTML = "a";
+
+
+    
+    
 }
 
