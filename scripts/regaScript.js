d517214ee5bc08d77dd4ad8d2835d744897821d2
Martim Paraíba <fc56273@aluno.fc.ul.pt>
2022-05-02 22:03:37 +0100

    Working Implementation of addRega

--- scripts/regaScript.js
+++ scripts/regaScript.js
@@ -4,29 +4,73 @@ function addRega(){
     const name          = document.getElementById("name").value;
     const init          = document.getElementById("init").value;
     const end           = document.getElementById("end").value;
-    const everyMonth    = document.getElementById("everyMonth").value;
+    const everyMonth    = document.getElementById("everyMonth").checked;
     const fromM         = document.getElementById("fromM").value;
     const toM           = document.getElementById("toM").value;
-    const everyDay      = document.getElementById("everyDay").value;
+    const everyDay      = document.getElementById("everyDay").checked;
     const fromD         = document.getElementById("fromD").value;
     const toD           = document.getElementById("toD").value;
 
-    document.getElementById("demo").innerHTML = name + init + end + everyMonth + fromM + toM + everyDay + fromD + toD;
 
     const listaRega = document.getElementById("listaRega");
-
-    li = document.createElement("li");
+    
     h4 = document.createElement("h4");
+    li = document.createElement("li");
     ul = document.createElement("ul");
 
-    timeP = document.createElement("p");
-    everyDayP = document.createElement("p");
-    everyMonthP = document.createElement("p");
-
-    h4.innerHTML = "a";
-
-
+    let timeP = document.createElement("p");
     
+    let everyMonthP = document.createElement("p");
+    let fromToM = document.createElement("span");
     
+    let everyDayP = document.createElement("p");
+    let fromToD = document.createElement("span");
+    timeP.innerText = init + " -- " + end;
+    
+    h4.innerText = name;
+
+
+    li.appendChild(h4);
+
+    ul.appendChild(timeP);
+
+    if (everyMonth) {
+        everyMonthP.innerHTML = "Todos os meses <input type=\"checkbox\" name=\"everyMonth\"checked disabled>";
+        ul.appendChild(everyMonthP);
+
+    } else {
+        everyMonthP.innerHTML = "Todos os meses <input type=\"checkbox\" name=\"everyMonth\" disabled>";
+        fromToM.innerText = fromM + " -- " + toM;
+        ul.appendChild(everyMonthP);
+        ul.appendChild(fromToM);
+    }
+
+    if (everyDay) {
+        everyDayP.innerHTML = "Todos os dias <input type=\"checkbox\" name=\"everyDay\" checked disabled>";
+        ul.appendChild(everyDayP);
+    } else {
+        everyDayP.innerHTML = "Todos os dias <input type=\"checkbox\" name=\"everyDay\" disabled>";
+        fromToD.innerText = fromD + " -- " + toD;
+        ul.appendChild(everyDayP);
+        ul.appendChild(fromToD);
+    }
+
+    li.appendChild(ul);
+    listaRega.appendChild(li);
+
 }
+
+function toggleAddRega() {
+    //elem = document.getElementById("rega");
+    //elem.setAttribute("hidden", false);
+
+    //elem = document.getElementById("addRega");
+    //elem.setAttribute("hidden", false);
+
+
+    hideToggleById("rega");
+    hideToggleById("addRega");
+}
+
+
 
