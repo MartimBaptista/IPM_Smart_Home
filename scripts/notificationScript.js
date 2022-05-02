a32953c8cea47b2faf75bf24978920943a5e7114
MartimBaptista <martimdsbaptista@gmail.com>
2022-05-02 22:40:51 +0100

    notification part done

--- scripts/notificationScript.js
+++ scripts/notificationScript.js
@@ -1,32 +1,48 @@
 var noticationList = document.getElementById("notificationList");
 
-function addNotification(arg) {
-    const text = document.createTextNode(arg);
+function addNotification(_title, _desc) {
+
+    //Title
+    const title = document.createElement("a");
+    title.appendChild(document.createTextNode(_title));
+    title.className = "title";
+    //Description
+    const desc = document.createElement("a");
+    desc.appendChild(document.createTextNode(_desc));
+    desc.className = "desc";
+    //Button
+    const button = document.createElement("input");
+    button.type = "button";
+    button.addEventListener("click", removeNotification);
+    //button.value = "OK";
+    
+    //Containers
+    const gridText = document.createElement("div");
+    gridText.className = "grid-text";
+    gridText.appendChild(title);
+    gridText.appendChild(desc);
+
+    const gridButton = document.createElement("div");
+    gridButton.className = "grid-button";
+    gridButton.appendChild(button);
+
+    const gridContainer = document.createElement("div");
+    gridContainer.className = "grid-container";
+    gridContainer.appendChild(gridText);
+    gridContainer.appendChild(gridButton);
+
     const item = document.createElement("li");
-    const button1 = document.createElement("input");
-
-    const br = document.createElement("br");
-
-    button1.type = "button";
-    button1.value = "Sim";
-    button1.addEventListener("click", removeNotification);
-
-    const button2 = document.createElement("input");
-    button2.type = "button";
-    button2.value = "Não";
-    button2.addEventListener("click", removeNotification);
+    item.appendChild(gridContainer);
     
-    
-    item.appendChild(text);
-    item.append(br)
-    item.append(button1);
-    item.append(button2);
+    //Adding it all up
     noticationList.appendChild(item);
 }
 
 function removeNotification() {
-    this.parentElement.remove();
+    this.parentElement.parentElement.parentElement.remove();
 }
 
 //Adding a starting notification for case use
-addNotification("Intruso detetado! Chamar autoridades?");
+addNotification("Intruso detetado!", "Um intruso foi detetado idk what else u want to know");
+
+//
